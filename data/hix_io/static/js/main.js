5714f1595dbca17ee948293d224e9690e92902cb
Michael B. Hix <mhix@laika.com>
2014-03-31 23:17:41 -0700

    Push post counting down to the handler to reduce complexity in the Can controls.

--- data/hix_io/static/js/main.js
+++ data/hix_io/static/js/main.js
@@ -27,37 +27,18 @@ function highlightSyntax() {
 }
 
 /*
- * Perform a synchronous HTTP request and return the body.
- */
-function syncReq(type, path) {
-	return function(params) {
-		var result = null;
-
-		$.ajax({
-			url: path,
-			type: 'get',
-			data: params,
-			dataType: 'json',
-			async: false,
-			success: function(data) {
-				result = data;
-			}
-		});
-
-		return result;
-	};
-}
-
-/*
  * Perform an asynchronous HTTP request and return the deferred result.
  */
-function asyncReq(type, path) {
+function asyncReq(path, method, type) {
+	if(!method) { method = 'GET'; }
+	if(!type) { type = 'json'; }
+
 	return function(params) {
 		return can.ajax({
-			url: '/api/v1/posts/search',
-			type: 'get',
+			url: path,
+			type: method,
 			data: params,
-			dataType: 'json'
+			dataType: type
 		});
 	};
 }
@@ -70,11 +51,9 @@ function asyncReq(type, path) {
  * Posts for a blog.
  */
 var Post = can.Model.extend({
-	findAll:      'GET /api/v1/posts',
-	findOne:      'GET /api/v1/posts/{id}',
-	count:        syncReq('GET', '/api/v1/posts/count'),
-	search:       asyncReq('GET', '/api/v1/posts/search'),
-	search_count: syncReq('GET', '/api/v1/posts/search/count'),
+	findOne: 'GET /api/v1/posts/{id}',
+	list:    asyncReq('/api/v1/posts'),
+	search:  asyncReq('/api/v1/posts/search'),
 }, {});
 
 /******************************************************************************/
@@ -83,58 +62,6 @@ var Post = can.Model.extend({
 
 /*
  * A reusable pager control.
- *
- * You provide a function that gets called when the pager is changed. The
- * callback must call update() on the pager in return so that the pager gets
- * re-rendered. This means that your view is in control - you can put the
- * pager's element inside another template if you wish.
- *
- * If you'd like to store the current page in a javascript cookie, make sure
- * to set the 'cookie' option when creating the pager. You'll also need the
- * jquery-cookie plugin, or a function called $.cookie() that works the same
- * way. 
- *
- * Basic usage:
- *
- * The basic idea is that this control will be used by other controls and it's
- * target element in the dom will be contained in or rendered by another
- * template.
- *
- * In your control's init funciton, create a new Pager, passing in the element
- * that your view is bound to or the one that contains the target element for
- * the pager, and the options you'd like. Be sure to include the callback
- * 'on_change' that re-renders your view. In that function, make sure to use
- * query_params() to get the current offset and limit to pass to findAll().
- *
- * Also, make sure to call update() on the pager in that function. For example:
- *
- *    init: function(element, options) {
- *        var self = this;
- *        
- *        this.pager = new Pager(element, {
- *            per_page: 5, 
- *            count_func: MyModel.count;
- *            on_change: function() { self.update(); },
- *        });
- *    },
- *    
- *    update: function() {
- *        var self = this;
- *        
- *        MyModel.findAll(this.pager.query_params(), function(items) {
- *            self.element.html(can.view('my_template.ejs', { items: items }));
- *            self.pager.update();
- *        });
- *    },
- *
- * In your template (my_template.ejs above), make sure to include the element
- * that your pager renders to. It's '#pager' by default.
- * 
- * Options you can pass to new Pager(element, options) are:
- *
- *     on_change: (required)
- *         The function to call when the pager is clicked and it
- *         results in a page change.
  *
  *     count_func: (required)
  *         The function to call to get the total number of items to be paged
@@ -149,19 +76,11 @@ var Post = can.Model.extend({
  *
  *     target: (optional, default: '#pager')
  *         The selector that yields one element to render the pager into.
- *
- *     cookie: (optional, default: null)
- *         The name of the javascript cookie that's set to retain the current
- *         page number. Set this to a simple string to enable the cookie.
- *
  */
 var Pager = can.Control.extend({
 	defaults: {
 		view: '/templates/pager.ejs',
 		target: '#pager',
-		count_func: null,
-		cookie: null,
-		on_change: null,
 	},
 },{
 	init: function(element, options) {
@@ -175,10 +94,6 @@ var Pager = can.Control.extend({
 
 		this.state.bind('page', options.on_change);
 
-		this.options.on_change = options.on_change;
-		this.options.count_func = options.count_func;
-		this.options.cookie = options.cookie;
-
 		if(options.target) { this.options.target = options.target; }
 
 		p = parseInt(options.per_page);
@@ -188,10 +103,13 @@ var Pager = can.Control.extend({
 		if(p > 0) { this.options.pad = p; }
 	},
 
-	update: function() {
-		p = this.state.page;
-		c = this.state.pages;
-		a = this.state.pad;
+	update: function(count) {
+		this.state.attr('count', count);
+		this.state.attr('pages', Math.ceil(this.state.count / this.state.per_page));
+
+		p = this.state.attr('page');
+		c = this.state.attr('pages');
+		a = this.state.attr('pad');
 
 		// Find the ammount we need to extend each side of the window to account for
 		// collapses on the opposite side. This allows for a roughly constant-width
@@ -208,37 +126,8 @@ var Pager = can.Control.extend({
 		$(this.options.target).html(can.view(this.options.view, this.state));
 	},
 
-	query_params: function(params) {
-
+	params: function(params) {
 		if(!params) { params = {}; }
-
-		// Attempt to update the count if we have something that doesn't make sense
-		// as a count.
-		//
-		if(this.state.count <= 0) {
-			this.state.attr('count', parseInt(this.options.count_func(params)));
-			this.state.attr('pages', Math.ceil(this.state.count / this.state.per_page));
-		}
-
-		// If enabled, use a JavaScript cookie to restore the last page viewed.
-		//
-		if(this.options.cookie) {
-			stored_page = parseInt($.cookie(this.options.cookie));
-
-			if(stored_page >= 0 &&
-			   stored_page < this.state.pages &&
-			   stored_page != this.state.page ) {
-
-				// Suppress the change event so the view isn't rendered multiple times.
-				//
-				this.state.unbind('page', this.options.on_change);
-				this.state.attr('page', stored_page);
-				this.state.bind('page', this.options.on_change);
-			} else {
-				$.cookie(this.options.cookie, this.state.page);
-			}
-		}
-
 		params.offset = this.state.page * this.state.per_page;
 		params.limit = this.state.per_page;
 		return params;
@@ -248,12 +137,8 @@ var Pager = can.Control.extend({
 		var page = parseInt(el.attr('data-page'));
 
 		if(page >= 0 && page < this.state.pages) {
-			if(this.options.cookie) {
-				$.cookie(this.options.cookie, page);
-			}
 			this.state.attr('page', page);
 		}
-
 	},
 });
 
@@ -265,10 +150,8 @@ var PostControl = can.Control.extend({}, {
 		var self = this;
 
 		this.pager = new Pager(element, {
-			per_page: 5, 
-			count_func: Post.count,
+			per_page: 5,
 			on_change: function() { self.update(); },
-			cookie: 'hix_io_post_page',
 		});
 
 		can.route('posts');
@@ -278,11 +161,13 @@ var PostControl = can.Control.extend({}, {
 	update: function() {
 		var self = this;
 
-		Post.findAll(this.pager.query_params(), function(posts) {
+		params = this.pager.params();
+
+		Post.list(params).done(function(data) {
 			self.element.html(can.view('/templates/posts.ejs', {
-				posts: posts
+				posts: Post.models(data.posts),
 			}));
-			self.pager.update();
+			self.pager.update(data.count);
 			highlightSyntax();
 		});
 	},
@@ -300,6 +185,42 @@ var PostControl = can.Control.extend({}, {
 			}));
 			highlightSyntax();
 		});
+	},
+});
+
+/*
+ * Provide a control for displaying search results.
+ */
+var SearchControl = can.Control.extend({}, {
+	init: function(element, options) {
+		var self = this;
+
+		this.q = null;
+		this.pager = new Pager(element, {
+			per_page: 5,
+			on_change: function() { self.update(); },
+		});
+
+		can.route('search');
+	},
+
+	update: function() {
+		var self = this;
+
+		params = this.pager.params({ q: this.q });
+
+		Post.search(params).done(function(data) {
+			self.element.html(can.view('/templates/search.ejs', {
+				q: params.q,
+				posts: Post.models(data.posts),
+			}));
+			self.pager.update(data.count);
+		});
+	},
+
+	'search route': function(data) {
+		this.q = data.q;
+		this.update();
 	},
 });
 
@@ -356,47 +277,6 @@ var MenuControl = can.Control.extend({
 				$(e).parent().removeClass(self.options.selected_class);
 			}
 		});
-	},
-});
-
-/*
- * Provide a control for displaying search results.
- */
-var SearchControl = can.Control.extend({}, {
-	init: function(element, options) {
-		var self = this;
-
-		this.q = '';
-		this.pager = new Pager(element, {
-			per_page: 5, 
-			count_func: Post.search_count,
-			on_change: function() { self.update(); },
-		});
-
-		window.pager = this.pager;
-
-		can.route('search');
-	},
-
-	update: function() {
-		var self = this;
-
-		params = this.pager.query_params({ q: this.q });
-
-		console.log(params);
-
-		Post.search(params).done( function(data) {
-			self.element.html(can.view('/templates/search.ejs', {
-				q: self.q,
-				posts: Post.models(data),
-			}));
-			self.pager.update();
-		});
-	},
-
-	'search route': function(data) {
-		this.q = data.q;
-		this.update();
 	},
 });
 
