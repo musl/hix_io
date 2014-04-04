aec73aad04fd32cdc8c833929df79ebbab993f0e
Michael B. Hix <mhix@laika.com>
2014-04-03 23:55:57 -0700

    Comment dataset methods, tidy up the full-text searching a bit.

--- lib/hix_io/models/post.rb
+++ lib/hix_io/models/post.rb
@@ -6,6 +6,13 @@ class HixIO::Post < Sequel::Model( :hix_io__posts )
 
 	dataset_module do
 
+		# Fetch a list of published posts.
+		#
+		# Recognized keys in +params+:
+		#
+		# :offset => Passed to the offset SQL clause.
+		# :limit  => Passed to the limit SQL clause.
+		#
 		def published( params = {} )
 			set = self.where( :published => true ).order( :updated_at )
 			set = set.offset( params[:offset] ) unless params[:offset].nil?
@@ -13,21 +20,35 @@ class HixIO::Post < Sequel::Model( :hix_io__posts )
 			return set
 		end
 
+		# Fetch a published post with the given +id+.
+		#
 		def detail( id )
 			return self.where( :id => id, :published => true )
 		end
 
+		# Perform a full-text search with a limited number of terms.
+		#
+		# Recognized keys in +params+:
+		#
+		# :q          => The query string.
+		# :offset     => Passed to the offset SQL clause.
+		# :limit      => Passed to the limit SQL clause.
+		# :language   => Language. defaults to 'english'
+		# :max_length => Queries longer than this will be truncated to this many
+		#                characters. defaults to 100
+		#
 		def search( params = {} )
-			# We'll trust sequel's quoting here, for now. I'd like to make sure it does
-			# the right thing at some point.
-			terms = [params[:q].to_s.split( /\s+/ )].flatten
-			opts  = {
-				:language => (params[:language] || 'english')
-			}
+			qmax = params[:max_length] || 100
+			opts = { :language => (params[:language] || 'english') }
+			terms = params[:q].to_s.slice( 0..qmax ).split( /[\s]+/ ).reject( &:empty? )
+
+			# FIXME there has to be a better way to return an empty set
+			return self.where( :id => -1 ) if terms.empty?
 
 			set = self.published.full_text_search( [:title, :body], terms, opts )
 			set = set.offset( params[:offset] ) unless params[:offset].nil?
 			set = set.limit( params[:limit] ) unless params[:limit].nil?
+
 			return set
 		end
 
