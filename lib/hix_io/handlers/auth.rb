b4061f419dd4517333e1360ad102a09fe20225bb
Michael B. Hix <mhix@laika.com>
2014-05-16 13:23:18 -0700

    Un-do a derpish commit about not requiring auth for get requests on /auth/. Hurr.

--- lib/hix_io/handlers/auth.rb
+++ lib/hix_io/handlers/auth.rb
@@ -21,7 +21,7 @@ class HixIO::Auth < Strelka::App
 
 	session_namespace :hix_io
 
-	require_auth_for { |r| r.verb == :DELETE }
+	require_auth_for { |r| %i[GET DELETE].include?( r.verb ) }
 	no_perms_for //
 
 	########################################################################
