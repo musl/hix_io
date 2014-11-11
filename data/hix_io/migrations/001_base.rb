0ce5c42408212f5dda8eac4ee0973812753b48a1
Michael B. Hix <mhix@laika.com>
2014-11-11 15:40:30 -0800

    Add db notes comment to the first migration.

--- data/hix_io/migrations/001_base.rb
+++ data/hix_io/migrations/001_base.rb
@@ -1,5 +1,15 @@
 # vim: set nosta noet ts=4 sw=4 ft=ruby:
 # encoding: UTF-8
+#
+# Notes on dev DB setup stuck here for a moment:
+#	create role hix_io with login;
+#	alter role hix_io set search_path to hix_io;
+#	create database hix_io_dev owner hix_io;
+#	grant all on database hix_io_dev to hix_io;
+#	\c hix_io_dev;
+#	create schema hix_io authorization hix_io;
+#	grant all on schema hix_io to hix_io;
+#
 Sequel.migration do
 
 	up do
